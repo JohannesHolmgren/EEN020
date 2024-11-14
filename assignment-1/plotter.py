14e865fcd5e9d52681f6cb4ccc11efe89e5f9f59
Johannes <johannes.h.holmgren@gmail.com>
2024-11-14 21:52:13 +0100

    Assignment 1 done

--- assignment-1/plotter.py
+++ assignment-1/plotter.py
@@ -80,7 +80,7 @@ def plot_points_2D(ax: plt.axes, points: np.ndarray, c=None) -> None:
     return ax
 
 # Function to plot coordinates in 3D
-def plot_points_3D(ax: plt.axes, points: np.ndarray) -> None:
+def plot_points_3D(ax: plt.axes, points: np.ndarray, c=None) -> None:
     ''' Plot points in 3D space
 
     Plots all 'n' points (3 x n) using matplotlib.
@@ -92,6 +92,9 @@ def plot_points_3D(ax: plt.axes, points: np.ndarray) -> None:
     
     points: np.ndarray
         A 3 x n matrix representing 'n' points
+
+    c: str (default None)
+        A color string to pass to matplotlib. If None then use y-value.
     
     Returns
     -------
@@ -99,13 +102,36 @@ def plot_points_3D(ax: plt.axes, points: np.ndarray) -> None:
         The surface plotted on. Can be used for further plotting
     
     '''
-    # fig = plt.figure()
-    # ax = fig.add_subplot(projection='3d')
-    ax.scatter(points[0,:], points[1,:], points[2,:], c=points[2,:])
+    ax.scatter(points[0,:], points[1,:], points[2,:], c=c)
     ax.set_aspect('equal')
     ax.set_xlabel('x')
     ax.set_ylabel('y')
-    # plt.show()    
+
+
+def plot_camera(ax: plt.axes, center: np.ndarray, axis: np.ndarray, scale: float) -> None:
+    ''' Plots the principal axis of a camera scaled by s starting from the camera center
+
+    Camera center and axis can be obtained from the function 'camera_center_and_axis'.
+
+    Parameters
+    ----------
+    ax: plt.axes
+        The matplotlib surface to plot on (from fig, ax = plt.subplots())
+
+    center: np.ndarray
+        The camera center
+
+    axis: np.ndarray
+        The camera's principal axis
+    
+    scale: float
+        The length (scale) of the camera principal axis    
+    
+    '''
+    ax.quiver(center[0], center[1], center[2], 
+              axis[0], axis[1], axis[2], 
+              length=scale, arrow_length_ratio=0.3, color='r'
+    )
 
 
 if __name__ == '__main__':
